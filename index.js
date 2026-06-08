30217195f18c288b89f33fe9b732884c36fa2148
Adebayorr <raheemadebayorr@gmail.com>
2026-06-08 13:01:27 +0100

    complete question 5

--- index.js
+++ index.js
@@ -7,37 +7,37 @@
 
 const students = [
     {
-        id: '983yt4',
+        id: 1,
         name: 'Olurotimi',
         age: 21,
         grades: [55, 67, 60]
     },
     {
-        id: '045dr1',
+        id: 2,
         name: 'Abubakar',
         age: 29,
         grades: [65, 70, 82]
     },
     {
-        id: '788dr1',
+        id: 3,
         name: 'Kelechi',
         age: 25,
         grades: [59, 69, 66]
     },
     {
-        id: '983nb2',
+        id: 4,
         name: 'Sonibare',
         age: 31,
         grades: [80, 89, 75]
     },
     {
-        id: '030ye4',
+        id: 5,
         name: 'Williams',
         age: 26,
         grades: [70, 67, 60]
     },
     {
-        id: '983yt4',
+        id: 6,
         name: 'Ephraim',
         age: 22,
         grades: [45, 57, 60]
@@ -54,14 +54,14 @@ const students = [
 
 function calculateAverage (grades) {
     let average = grades.reduce((acc, curr) => acc + curr, 0) / grades.length
-    return average.toFixed(2)
+    return Number(average.toFixed(2))
 }
 
-students.map((student) => {
-    student.average  = calculateAverage(student.grades)
-    console.log(student)
+const studentsWithAverage = students.map((student) => {
+    let average  = calculateAverage(student.grades)
+    return {...student, average}
 })
-
+console.log(studentsWithAverage)
 
 /*
 ==================
@@ -71,7 +71,7 @@ students.map((student) => {
 
 const getPassingStudents = (students) => students.filter((student) => student.average >= 60 )
 
-const passing = getPassingStudents(students);
+const passing = getPassingStudents(studentsWithAverage);
 console.log(passing);
 
 
@@ -92,43 +92,36 @@ function addLetterGrade (student) {
     let {id, name, average} = student
 
     if  (score >= 90 ) {
-        student.letterGrade = 'A'
-        let {letterGrade} = student
-        return {id, name, average, letterGrade}
+        return {id, name, average, 'letterGrade': 'A'}
     } else if (score >= 80) {
-        student.letterGrade = 'B'
-        let {letterGrade} = student
-        return {id, name, average, letterGrade}
+        return {id, name, average, 'letterGrade': 'B'}
     } else if (score >= 70) {
-        student.letterGrade = 'C'
-        let {letterGrade} = student
-        return {id, name, average, letterGrade}
+        return {id, name, average, letterGrade: 'C'}
     } else if (score  >= 60) {
-        student.letterGrade = 'D'
-        let {letterGrade} = student
-        return {id, name, average, letterGrade}
+        return {id, name, average, letterGrade: 'D'}
     } else {
-        student.letterGrade = 'F'
-        let {letterGrade} = student
-        return {id, name, average, letterGrade}
+        return {id, name, average, letterGrade: 'F'}
     }
 }
 
 
 function addStatus (student) {
     let {average, name, id} = student
-    student.status = average > 60 ? 'Pass' : 'Fail'
-    let {status} = student
-    console.log(status)
-    return {id, name, average, status}
+    return {
+        id, 
+        name, 
+        average, 
+        status: average >= 60 ? 'Pass' : 'Fail'
+    }
 }
 
 
-const studentWithStatus = processStudents(students, addStatus)
-console.log(studentWithStatus)
-
-const studentWithLetterGrade = processStudents(students, addLetterGrade)
+const studentWithLetterGrade = processStudents(studentsWithAverage, addLetterGrade)
 console.log(studentWithLetterGrade)
+
+const studentWithStatus = processStudents(studentsWithAverage, addStatus)
+console.log(studentWithStatus)
+console.log(students);
 
 
 
@@ -139,8 +132,8 @@ console.log(studentWithLetterGrade)
 */
 
 const findStudentById = (students, id) => {
-    students.find()
+    return students.find(student => student.id === id) || null
 }
 
-const student = findStudentById(students, '030ye4')
+const student = findStudentById(students, 1)
 console.log(student);
